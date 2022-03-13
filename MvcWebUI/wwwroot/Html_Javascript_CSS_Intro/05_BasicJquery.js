1e7dffcc44b35ace0a0c927d1edc85f294687446
Simge Işık <simgeiirem@outlook.com>
2022-03-13 19:37:47 +0300

    13 Mart

---
+++ MvcWebUI/wwwroot/Html_Javascript_CSS_Intro/05_BasicJquery.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    console.log("script loaded");
+    var inputText; //textbox ın değerini okuyup kullanmak için aldık
+
+
+    $("#bSetText").click(function () {
+        inputText = $("#tbInput1").val(); //textbox, dropdownlist, listbox val ile okunur
+        $("#sOutput1").text(inputText); //span, label, p text ile okunur        
+    });
+    $("#bSetHtml").click(function () {
+        inputText = $("#tbInput1").val();
+        $("#sOutput2").html('<label style="color:purple;font-size:24px;"><b>' + inputText + '</b></label>');
+    });
+
+    $("#bSetValue").click(function () {
+        inputText = $("#tbInput1").val();
+        $("#tbOutput1").val(inputText);
+    })
+
+});
